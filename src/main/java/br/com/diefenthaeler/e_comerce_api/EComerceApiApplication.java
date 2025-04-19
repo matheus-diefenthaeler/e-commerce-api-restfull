3a81cb048d443742ecb2258a6b6cf428b2c1162f
Matheus Diefenthaeler <matheus.axo@gmail.com>
2025-04-19 14:58:44 -0300

    feat: Project add.

---
+++ src/main/java/br/com/diefenthaeler/e_comerce_api/EComerceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.diefenthaeler.e_comerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComerceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diefenthaeler/e_comerce_api/EComerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diefenthaeler.e_comerce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
